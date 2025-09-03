2cff609772ef80a057a534d305166c9c0b033799
Darwyn01 <darwyn.avila001@albright.edu>
2025-09-03 09:04:45 -0400

    Add files via upload

---
+++ CSC_141 MAIN/Assignment 2/Assignment_2_Answers.py
@@ -0,0 +1,47 @@
+# Darwyn Avila-Medina
+#2-1
+message = ("How are you Python world?") 
+print(message) 
+#2-2
+message = ("Sup Python world!")
+print (message) 
+#2-3
+name = "Bruno"
+message = (f"Hello, {name.title()}!")
+print(message, "How are you?")
+#2-4 Comment --> Darwyn Avila-Medina, 9/3/25. This program prints a famous persons name in lower, upper, and title case. 
+name= "Bruno"
+print (name.upper())
+print (name.lower())
+print (name.title())
+#2-5  Comment --> Darwyn Avila-Medina, 9/3/25. This program prints a famous quote from Cristiano Ronaldo. 
+first_name = "Cristiano"
+last_name = "Ronaldo"
+full_name = f"{first_name} {last_name}"
+message = ('once said, "Talent without hard work is nothing."')
+print (full_name, (message))
+#2-6
+famous_person = "Cristiano Ronaldo"
+message = ('once said, "Talent without hard work is nothing."')
+print (famous_person, message)
+#2-7
+name = ' Bryan '
+print ("\t Bryan ")
+print ("\n Bryan ")
+print (name.rstrip())
+print (name.lstrip())
+print (name.strip())
+#2-8
+filename = 'python_notes.txt'
+print (filename.removesuffix('.txt'))
+#2-9
+print (4+4)
+print (12-4)
+print (2*4)
+print (16/2)
+#2-10
+numero = "27"
+message = ('My favorite number is')
+print (message, numero)
+#2-11
+#Comments are in problems 2-4 and 2-5. 
